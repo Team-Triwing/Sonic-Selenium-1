5b54e1662e07f903e89ac768f3e59bfff86ae0c9
Jacob T. Andersen <jacobjaretts@gmail.com>
2021-05-03 06:56:27 -0700

    star light zone compression

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 "tools/asm68k.exe" /o ae-,op+,os+,ow+,oz+,oaq+,osq+,omq+,w- /m /c /e RecordableDemo=0 /q /p main.asm, out.md, , out.lst>out.log
 type out.log
 if not exist out.md pause & exit
-"AMPS\Dual PCM Compress.exe" AMPS\.z80 AMPS\.z80.dat out.md _dlls\lzkn.exe
+"AMPS\Dual PCM Compress.exe" AMPS\.z80 AMPS\.z80.dat out.md _dlls\slz.exe
 error\convsym out.lst out.md -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
 "tools/fixheadr.exe" out.md
 del out.log
