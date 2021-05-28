cd760f7adf9fc2af936ad83f805ff7ed385ee6ec
Jacob T. Andersen <jacobjaretts@gmail.com>
2021-05-28 06:35:10 -0700

    changed ROM file extension

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 @echo off
 "AMPS\Includer.exe" ASM68K AMPS AMPS\.Data
-"tools/asm68k.exe" /o ae-,op+,os+,ow+,oz+,oaq+,osq+,omq+,w- /m /c /e safe=0 /e RecordableDemo=0 /q /p main.asm, out.md, , out.lst>out.log
+"tools/asm68k.exe" /o ae-,op+,os+,ow+,oz+,oaq+,osq+,omq+,w- /m /c /e safe=0 /e RecordableDemo=0 /q /p main.asm, out.gen, , out.lst>out.log
 type out.log
-if not exist out.md pause & exit
-"AMPS\Dual PCM Compress.exe" AMPS\.z80 AMPS\.z80.dat out.md _dlls\saxcmp.exe
-error\convsym out.lst out.md -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
-"tools\romfix.exe" -t "Sonic Selenium" -d -r 00 -s 050104RM -z out.md
+if not exist out.gen pause & exit
+"AMPS\Dual PCM Compress.exe" AMPS\.z80 AMPS\.z80.dat out.gen _dlls\saxcmp.exe
+error\convsym out.lst out.gen -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
+"tools\romfix.exe" -t "Sonic Selenium" -d -r 00 -s 050104RM -z out.gen
 del out.log
 pause
--- build_recordabledemo.cmd
+++ build_recordabledemo.cmd
@@ -1,10 +1,10 @@
 @echo off
 "AMPS\Includer.exe" ASM68K AMPS AMPS\.Data
-"tools/asm68k.exe" /o ae-,w- /e RecordableDemo=1 /e safe=1 /q /p main.asm, out_demo.md, , out.lst>out.log
+"tools/asm68k.exe" /o ae-,w- /e RecordableDemo=1 /e safe=1 /q /p main.asm, out_demo.gen, , out.lst>out.log
 type out.log
-if not exist out_demo.md pause & exit
-"AMPS\Dual PCM Compress.exe" AMPS\.z80 AMPS\.z80.dat out_demo.md _dlls\saxcmp.exe
-error\convsym out.lst out_demo.md -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
-"tools\romfix.exe" -t "Sonic Selenium (demo, not for release)" -d -s "DEMO" -r 00 -z out_demo.md
+if not exist out_demo.gen pause & exit
+"AMPS\Dual PCM Compress.exe" AMPS\.z80 AMPS\.z80.dat out_demo.gen _dlls\saxcmp.exe
+error\convsym out.lst out_demo.gen -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
+"tools\romfix.exe" -t "Sonic Selenium (demo, not for release)" -d -s "DEMO" -r 00 -z out_demo.gen
 del out.log
 pause
