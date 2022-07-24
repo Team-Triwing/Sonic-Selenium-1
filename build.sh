b456bd1cd5296e25a3420b8045a95fc90e5aa88f
RepellantMold <repellantmold@outlook.com>
2022-07-24 15:51:29 -0700

    add working build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wineconsole "AMPS/Includer.exe" ASM68K AMPS AMPS/.Data
-wineconsole "tools/asm68k.exe" /o ae-,op+,os+,ow+,oz+,oaq+,osq+,omq+,w-,l. /m /c /e safe=0 /q /p "main.asm", "Sonic_Selenium_1.gen", , "out.lst">"out.log"
+mono "AMPS/Includer.exe" ASM68K AMPS AMPS\.Data
+wineconsole "tools/asm68k.exe" /o ae-,op+,os+,ow+,oz+,oaq+,osq+,omq+,w-,l. /m /c /e safe=0 /q /p "main.asm", "Sonic_Selenium_1.gen", , "out.lst">out.log
 cat out.log
-wineconsole "AMPS\Dual PCM Compress.exe" "AMPS\.z80" "AMPS\.z80.dat" "Sonic_Selenium_1.gen" "_dlls\salvador.exe"
+mono "AMPS/Dual PCM Compress.exe" "AMPS/.z80" "AMPS/.z80.dat" "Sonic_Selenium_1.gen" "_dlls/salvador"
 ./error/convsym out.lst Sonic_Selenium_1.gen -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+ /processLocals+" -a
 ./tools/romfix -t "Sonic Selenium" -d -r 00 -s 050104RM -z Sonic_Selenium_1.gen
-#rm -f out.log
+rm -f out.log
